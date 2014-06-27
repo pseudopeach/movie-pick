82743c75f38fdba2c59b9fd48c03ac58f860666e
Justin Armstrong <pseudopeach@gmail.com>
2014-06-27 08:29:50 -0700

    extracts references_doc to libs

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Noar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    puts "autoloading lib? #{Rails.root.join('lib')}"
+    config.autoload_paths << Rails.root.join('lib')
+    puts "autoload #{config.autoload_paths}"
   end
 end
---
+++ lib/utiloid.rb
@@ -0,0 +1,44 @@
+module Utiloid
+  def self.included receiver
+    receiver.extend ClassMethods
+  end
+
+  module ClassMethods
+    def references_doc(name, options={})
+      name_s = name.to_s
+      key_name = (name_s+"_id")
+      class_name = options[:class_name]
+
+      attr_accessor key_name
+
+      #getter
+      define_method name_s do
+        @ref_cache ||= {}
+        return nil unless key_value = self.send(key_name)
+
+        if @ref_cache.key? name_s
+          #value found in instance cache
+          value_obj = @ref_cache[name_s]
+        else
+          #need to look up from db, then cache
+          value_obj = class_name.find(key_value)
+          @ref_cache[name_s] = value_obj
+        end
+
+        return nil if value_obj._id != key_value #stale cached value, don't return it
+        return value_obj
+      end
+
+      #setter
+      define_method (name_s+"=") do |value_obj|
+        @ref_cache ||= {}
+        #set key field
+        self.send(key_name+"=", value_obj._id)
+        @ref_cache[name_s] = value_obj
+      end
+
+    end #ref
+
+  end #class methods
+
+end
